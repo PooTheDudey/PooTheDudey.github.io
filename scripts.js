684829742b8d2eacca5245578910d1da6bf89e4d
Pooja Bahuguna <pbahugun@purdue.edu>
2024-09-08 22:57:20 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,34 +1,12 @@
-// Some code thanks to @chrisgannon
+/*
+Inspired from the design made by 'Teodora':
+https://www.webdesignerforum.co.uk/files/file/63-free-psd-cv-template/
+https://dribbble.com/shots/1141520-PSD-CV-template?list=searches&offset=17
 
-var select = function(s) {
-  return document.querySelector(s);
-}
+Dark-wall pattern: https://subtlepatterns.com/dark-wall/
 
-function randomBetween(min,max)
-{
-    var number = Math.floor(Math.random()*(max-min+1)+min);
-  
-    if ( number !== 0 ){
-      return number;
-    }else {
-      return 0.5;
-    }
-}
+Lato Font: https://www.google.com/fonts/specimen/Lato
 
-var tl = new TimelineMax();
+We love font icons: http://weloveiconfonts.com/
 
-for(var i = 0; i < 20; i++){
-
-  var t = TweenMax.to(select('.bubble' + i), randomBetween(1, 1.5), {
-    x: randomBetween(12, 15) * (randomBetween(-1, 1)),
-    y: randomBetween(12, 15) * (randomBetween(-1, 1)), 
-    repeat:-1,
-    repeatDelay:randomBetween(0.2, 0.5),
-    yoyo:true,
-    ease:Elastic.easeOut.config(1, 0.5)
-  })
-
-  tl.add(t, (i+1)/0.6)
-}
-
-tl.seek(50);
+*/
