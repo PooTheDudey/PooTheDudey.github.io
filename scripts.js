144b28904dee70930a89df24458091bb191b1810
Pooja Bahuguna <pbahugun@purdue.edu>
2024-09-08 23:51:49 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -8,29 +8,7 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
 });
 
-// Skill bar animations
-window.addEventListener('scroll', function() {
-    var skillBars = document.querySelectorAll('.bar-container');
-    skillBars.forEach(function(bar) {
-        var windowHeight = window.innerHeight;
-        var elementTop = bar.getBoundingClientRect().top;
-        var percent = bar.getAttribute('data-percent');
-        if (elementTop < windowHeight) {
-            bar.querySelector('.progressbar').style.width = percent + '%';
-        }
-    });
-});
-
 // Show experience content on scroll
 window.addEventListener('scroll', function() {
     var hiddenExperiences = document.querySelectorAll('.experience-content.hidden');
-    hiddenExperiences.forEach(function(exp) {
-        var windowHeight = window.innerHeight;
-        var elementTop = exp.getBoundingClientRect().top;
-        if (elementTop < windowHeight) {
-            exp.classList.remove('hidden');
-            exp.style.opacity = 1;
-            exp.style.marginLeft = '0';
-        }
-    });
-});
+    hiddenExperiences.forEach(function
